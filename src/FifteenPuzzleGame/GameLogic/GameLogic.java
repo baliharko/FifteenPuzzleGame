8bc637e6902fde39a20f548cae0854e29006497b
baliharko <bali@fauxdelorean.com>
2020-10-23 13:24:45 +0200

    changed header

--- src/FifteenPuzzleGame/GameLogic/GameLogic.java
+++ src/FifteenPuzzleGame/GameLogic/GameLogic.java
@@ -1,7 +1,7 @@
 package FifteenPuzzleGame.GameLogic;
 
 /**
- * Created by Oscar Norman <br>
+ * Created by Oscar Norman & Bali Harko <br>
  * Date: 2020-10-23   <br>
  * Time: 12:02   <br>
  * Project: GameWindow.java <br>
