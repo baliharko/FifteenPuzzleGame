066bda92952421cf6b6e15ed6790300eda8e3d59
baliharko <bali@fauxdelorean.com>
2020-10-23 11:49:06 +0200

    deleted everything 2

--- src/FifteenPuzzleGame/Main.java
+++ src/FifteenPuzzleGame/Main.java
@@ -1,9 +1,11 @@
-package FifteenPuzzleGame;/**
-*FifteenPuzzleGame <br>
-*baliharko <br>
-*2020-10-23 <br>
-*11:47
-*/
-    
+package FifteenPuzzleGame;
+
+/**
+ * FifteenPuzzleGame <br>
+ * baliharko <br>
+ * 2020-10-23 <br>
+ * 11:47
+ */
+
 public class Main {
 }
