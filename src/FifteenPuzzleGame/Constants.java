285abcfc403729968fefd4e9f428216c651e909c
baliharko <bali@fauxdelorean.com>
2020-10-23 13:50:22 +0200

    started on GUI

---
+++ src/FifteenPuzzleGame/Constants.java
@@ -0,0 +1,21 @@
+package FifteenPuzzleGame;
+
+/**
+ * FifteenPuzzleGame <br>
+ * baliharko & Oscar Norman <br>
+ * 2020-10-23 <br>
+ * 13:36
+ */
+
+public class Constants {
+
+    public static final int ROWS = 4;
+    public static final int COLUMNS = 4;
+    public static final int SCREEN_WIDTH = 800;
+    public static final int SCREEN_HEIGHT = 800;
+
+    public static final String TITLE = "Fifteen Puzzle Game";
+
+
+
+}
--- src/FifteenPuzzleGame/SwingGUI/GameWindow.java
+++ src/FifteenPuzzleGame/SwingGUI/GameWindow.java
@@ -1,11 +1,48 @@
 package FifteenPuzzleGame.SwingGUI;
 
+import static FifteenPuzzleGame.Constants.*;
+
+import javax.swing.*;
+import java.awt.*;
+
 /**
  * FifteenPuzzleGame <br>
- * baliharko <br>
+ * baliharko och Oscar Norman <br>
  * 2020-10-23 <br>
  * 12:20
  */
 
-public class GameWindow {
+public class GameWindow extends JFrame {
+
+    private JPanel mainPanel = new JPanel();
+    private JButton[] buttons;
+
+    public GameWindow() {
+        this.setSize(SCREEN_WIDTH, SCREEN_HEIGHT);
+        this.setResizable(false);
+        this.setLocationRelativeTo(null);
+        this.setTitle(TITLE);
+
+        buttons = new JButton[ROWS * COLUMNS];
+
+        mainPanel.setLayout(new GridLayout(ROWS, COLUMNS));
+
+        // Temporary
+        int i = 1;
+        for (JButton b : buttons) {
+
+            JButton temp = new JButton("" + i++);
+
+            mainPanel.add(temp);
+        }
+
+        this.add(mainPanel);
+
+        this.setVisible(true);
+        this.setDefaultCloseOperation(EXIT_ON_CLOSE);
+    }
+
+    public static void main(String[] args) {
+        GameWindow w = new GameWindow();
+    }
 }
